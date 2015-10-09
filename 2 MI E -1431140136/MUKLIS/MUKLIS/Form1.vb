275364cb81558e8239e7846a947f12a57a38a602
muklisnur <entis.hidayat@gmail.com>
2015-10-09 17:44:34 +0700

    Ganti warna

---
+++ 2 MI E -1431140136/MUKLIS/MUKLIS/Form1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If MyBase.BackColor = Color.Gold Then
+            MyBase.BackColor = Color.BlueViolet
+        Else : MyBase.BackColor = Color.Gold
+        End If
+
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
